0b0214c81f23fe0e7953baa846e275e30fe2f9eb
Tran Quang Trieu <trieutq3@fpt.com.vn>
2021-09-01 13:16:23 +0700

    add css

---
+++ v3/js/tracking_handlers.js
@@ -0,0 +1,17 @@
+window.trackingApp=new Tracking({debug:true,handlers(){this.on('DirectApplyFB',({fbq},payload)=>{fbq('track','DirectApplyFB');})
+this.on('EarnedApplyFB',({fbq},payload)=>{fbq('track','EarnedApplyFB');})
+this.on('AllApplyFB',({fbq},payload)=>{fbq('track','AllApplyFB');})
+this.on('AllApply',({gtag},payload)=>{initTracker();ga(tracker+'.send','event','AllApply','AllApply',1);})
+this.on('AllApplyGG',({gtag},payload)=>{gtag('event','AllApplyGG',{'send_to':'AW-823531084/j5XYCJ2gmdgBEMys2IgD'});})
+this.on('AllApplyTiktok',({ttq},payload)=>{ttq.instance('C3DCKS2TO1NMNN16KQ1G').track('AllApplyTiktok');})
+this.on('NewCV',({ga},payload)=>{initTracker();ga(tracker+'.send','event','NewCV','NewCV',1);})
+this.on('NewCVFB',({fbq},payload)=>{fbq('track','NewCVFB');})
+this.on('NewCVGG',({gtag},payload)=>{gtag('event','NewCVGG',{'send_to':'AW-823531084/MJAZCIakmdgBEMys2IgD'});})
+this.on('NewCVTiktok',({ttq},payload)=>{ttq.instance('C3DCKS2TO1NMNN16KQ1G').track('NewCVTiktok');})
+this.on('NewUser',({ga},payload)=>{initTracker();ga(tracker+'.send','event','NewUser','NewUser',1);})
+this.on('NewUserFB',({fbq},payload)=>{fbq('track','NewUserFB');})
+this.on('NewUserGG',({gtag},payload)=>{gtag('event','NewUserGG',{'send_to':'AW-823531084/YRfKCIPOp9gBEMys2IgD'});})
+this.on('NewUserTiktok',({ttq},payload)=>{ttq.instance('C3DCKS2TO1NMNN16KQ1G').track('NewUserTiktok');})}})
+$(function(){if(trackingQueue){setTimeout(function(){trackingApp.setQueue(trackingQueue).processQueue()},3000)}
+$(document).ajaxSuccess(function(event,xhr,settings){setTimeout(function(){let response=xhr.responseJSON
+if(response&&response.tracking_queue){trackingApp.setQueue(response.tracking_queue).processQueue()}},500)})})
